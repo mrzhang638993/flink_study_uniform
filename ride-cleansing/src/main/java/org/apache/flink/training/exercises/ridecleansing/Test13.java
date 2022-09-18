174afed477bb464294572789d212489039dd3fde
mrzhang638993 <1446719386@qq.com>
2022-09-18 15:32:44 +0800

    增加window的join操作功能实现和需求体现

--- ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/Test13.java
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/Test13.java
@@ -12,6 +12,7 @@ import org.apache.flink.util.OutputTag;
 
 //evict可以实现元素的移除操作。结合实现元素的各种功能新的替代操作实现的,可以实现更加丰富的功能实现。
 //globalWindow_trigger 可以实现自定义各种类型的window实现规则的,是很关键的操作的。
+//基于state可以实现更多的功能和实现的，globalWindow对应的定义了普通的规则实现的,需要把握的。
 public class Test13 {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
@@ -30,7 +31,7 @@ public class Test13 {
         SingleOutputStreamOperator<SocketPo> sum = source.windowAll(GlobalWindows.create())
                 //globalWindow配置触发器实现触发操作实现，很关键的要素操作
                 //.trigger(CountTrigger.of(3))
-                //自定义触发器实现
+                //自定义触发器实现，计算每隔两个单词计算三个单词的操作，跳跃操作的。使用trigger触发计算，计算完成在evict中剔除三个元素。
                 .trigger(new GlobalWindowTrigger())
                 .sum(1)
                 .uid("sum")
---
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/Test14.java
@@ -0,0 +1,8 @@
+package org.apache.flink.training.exercises.ridecleansing;
+
+//对应的滚动窗口之内的join操作和实现机制,需要特别的关注和实现操作
+public class Test14 {
+    public static void main(String[] args) {
+
+    }
+}
